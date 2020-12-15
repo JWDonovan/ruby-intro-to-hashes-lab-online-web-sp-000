ca88141020629aeca8cdf50ea84ea15a9869a3db
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 15:46:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ def monopoly_with_second_tier
     :railroads => {}
   }
 
-  monopoly[:railroads] = { :pieces => 4 }
+  monopoly[:railroads][0] = { :pieces => 4 }
+  monopoly[:railroads][1] = { :names => {} }
+  monopoly[:railroads][2] = { :rent_in_dollars => {} }
 end
 
 def monopoly_with_third_tier
